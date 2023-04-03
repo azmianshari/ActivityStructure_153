ee05b09e032e9b75b483ff443b12a600ae76ba0b
azmianshari <azmianshari123@gmail.com>
2023-04-03 15:05:26 +0700

    Menampilkan data mahasiswa

--- Structure2/Structure2.cpp
+++ Structure2/Structure2.cpp
@@ -29,4 +29,10 @@ int main() {
 	cout << "Masukan Umur : ";
 	cin >> mhs.umur;
 
+	cout << "\nNim : " << mhs.nim;
+	cout << "\nNama : " << mhs.nama;
+	cout << "\nDesa : " << mhs.alamat.desa;
+	cout << "\nKota : " << mhs.alamat.kota;
+	cout << "\nUmur : " << mhs.umur;
+
 }
