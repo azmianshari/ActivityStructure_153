77bf3a1fbef8e654d1dddde4ab6c94c889fc1be8
azmianshari <azmianshari123@gmail.com>
2023-04-03 14:27:07 +0700

    Membuat  struktur data

--- ActivityStructure/ActivityStructure.cpp
+++ ActivityStructure/ActivityStructure.cpp
@@ -4,11 +4,15 @@ using namespace std;
 struct Mahasiswa {
 	string nim;
 	string nama;
-	string alamt;
+	string alamat;
 	int umur;
 };
 
 int main()
 {
 	Mahasiswa mhs1, mhs2;
+	mhs1.nim = "20220140153";
+	mhs1.nama = "Muhammad Azmi Anshari";
+	mhs1.alamat = "Tasikmalaya";
+	mhs1.umur = 19;
 }
